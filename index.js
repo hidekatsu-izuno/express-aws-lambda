73acdab2d0790f610911143b106a45e86958e43c
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2019-02-25 07:29:52 +0900

    Add express request/response

--- index.js
+++ index.js
@@ -8,8 +8,18 @@
 
 const IncomingMessage = require('./lib/incoming_message');
 const ServerResponse = require('./lib/server_response');
+const request = require('./lib/request');
+const response = require('./lib/response');
 
 module.exports = function serverless(app) {
+    app.request = Object.create(request, {
+        app: { configurable: true, enumerable: true, writable: true, value: app }
+    });
+
+    app.response = Object.create(response, {
+        app: { configurable: true, enumerable: true, writable: true, value: app }
+    });
+
     return function handler(event, context, callback) {
         const req = new IncomingMessage(event);
         const res = new ServerResponse();
